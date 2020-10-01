68197a96e37964a9d24298b82a4ce7d22d8dc7b4
Yizeng Han <yizeng38@gmail.com>
2020-10-01 22:19:10 +0800

    update threads run code

--- threads_run.py
+++ threads_run.py
@@ -32,8 +32,16 @@ parser.add_argument('--train_url', type=str, metavar='PATH', default='./log/test
                     help='path to save result and checkpoint (default: results/savedir)')
 parser.add_argument('--init_method', type=str, default='',
                     help='an argument needed in huawei cloud, but i do not know its usage')
-parser.add_argument('--patch_groups', type=int, default=1,
+
+parser.add_argument('--patch_groups1', type=int, default=1,
                     help='an argument needed in huawei cloud, but i do not know its usage')
+parser.add_argument('--patch_groups2', type=int, default=1,
+                    help='an argument needed in huawei cloud, but i do not know its usage')
+parser.add_argument('--target_rate1', type=int, default=0.5,
+                    help='an argument needed in huawei cloud, but i do not know its usage')
+parser.add_argument('--target_rate2', type=int, default=0.5,
+                    help='an argument needed in huawei cloud, but i do not know its usage')
+
 parser.add_argument('--use_ls', type=int, default=0,
                     help='an argument needed in huawei cloud, but i do not know its usage')
 args = parser.parse_args()
@@ -41,7 +49,8 @@ args = parser.parse_args()
 args.use_ls = True if args.use_ls > 0 else False
 
 def multi_thread_run(config='_sarResNet50_g1_blConfig', 
-                    train_url_base='', 
+                    train_url_base='',
+                    patch_groups=1, 
                     data_url='',
                     dist_url='127.0.0.1:10001',
                     lambda_act=1.0,
@@ -52,7 +61,8 @@ def multi_thread_run(config='_sarResNet50_g1_blConfig',
                     test_code=0,
                     gpu='0,1,2,3'):
     ta_str = str(target_rate)
-    train_url = f'{train_url_base}target{ta_str[-1]}/'
+    ls = 1 if args.use_ls else 0
+    train_url = f'{train_url_base}g{patch_groups}_target{ta_str[-1]}_ls{ls}/'
     cmd = f'CUDA_VISIBLE_DEVICES={gpu} python sarNet/main_sar.py   \
             --train_url {train_url} \
             --data_url {data_url} \
@@ -69,8 +79,10 @@ def multi_thread_run(config='_sarResNet50_g1_blConfig',
     return cmd
 
 class myThread(threading.Thread):
-    def __init__(self, threadID=1, config='_sarResNet50_g1_blConfig', 
+    def __init__(self, threadID=1, 
+                config='_sarResNet50_g1_blConfig', 
                 train_url_base='', 
+                patch_groups=1,
                 data_url='',
                 dist_url='127.0.0.1:10001',
                 lambda_act=1.0,
@@ -83,6 +95,7 @@ class myThread(threading.Thread):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.config = config
+        self.patch_groups = patch_groups
         self.train_url_base = train_url_base
         self.data_url = data_url
         self.dist_url = dist_url
@@ -98,6 +111,7 @@ class myThread(threading.Thread):
         print ("start" + str(self.threadID))
         cmd = multi_thread_run(config=self.config, 
                 train_url_base=self.train_url_base, 
+                patch_groups=self.patch_groups,
                 data_url=self.data_url,
                 dist_url=self.dist_url,
                 lambda_act=self.lambda_act,
@@ -125,18 +139,21 @@ class myThread(threading.Thread):
 
 # gpu = ['0,1,2,3', '4,5,6,7']
 
-config = f'_sarResNet50_g{args.patch_groups}_blConfig'
+config1 = f'_sarResNet50_g{args.patch_groups1}_blConfig'
+config2 = f'_sarResNet50_g{args.patch_groups2}_blConfig'
 if args.use_ls:
-    config += '_ls'
+    config1 += '_ls'
+    config2 += '_ls'
 
 t1 = myThread(threadID=1,
-                config=config, 
+                config=config1, 
+                patch_groups=args.patch_groups1,
                 train_url_base=args.train_url, 
                 data_url=args.data_url,
                 dist_url=f'tcp://127.0.0.1:30076',
                 lambda_act=1.0,
                 t0=0.5,
-                target_rate=0.5,
+                target_rate=args.target_rate1,
                 optimize_rate_begin_epoch=55,
                 use_amp=1,
                 test_code=0,
@@ -144,13 +161,14 @@ t1 = myThread(threadID=1,
 t1.start()
 
 t2 = myThread(threadID=2,
-                config=config, 
+                config=config2, 
                 train_url_base=args.train_url, 
+                patch_groups=args.patch_groups2,
                 data_url=args.data_url,
                 dist_url=f'tcp://127.0.0.1:30075',
                 lambda_act=1.0,
                 t0=0.5,
-                target_rate=0.7,
+                target_rate=args.target_rate2,
                 optimize_rate_begin_epoch=55,
                 use_amp=1,
                 test_code=0,
