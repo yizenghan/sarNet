ab4c5a78d6dd3ee20fcdde3495102a530dc033e1
Yizeng Han <yizeng38@gmail.com>
2020-10-08 09:31:40 +0800

    add g1nomask_alpha2beta1

---
+++ configs/_sarResNet50_mask1_g1_alpha2_beta1_blConfig_ls.py
@@ -0,0 +1,16 @@
+model = dict(
+    type='models.sar_resnet_nomask',
+    num_classes=1000,
+    depth=50,
+    patch_groups=1,
+    alpha=2,
+    beta=1
+)
+train_cfg = dict(
+    hyperparams_set_index=333,
+    crop_size=224,
+)
+test_cfg = dict(
+    crop_size=224,
+    crop_type='normal'
+)
