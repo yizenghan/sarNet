cc983996047e61a1ae07f93eded3d8317d152a96
Yizeng Han <yizeng38@gmail.com>
2020-10-13 15:54:23 +0800

    add a2b1 ls+wmup settings

---
+++ configs/_sarResNet50_g2_a2b1_blConfig_ls_warmup.py
@@ -0,0 +1,16 @@
+model = dict(
+    type='models.sar_resnet_alpha',
+    num_classes=1000,
+    depth=50,
+    mask_size=7,
+    patch_groups=2,
+    alpha=2
+)
+train_cfg = dict(
+    hyperparams_set_index=233,
+    crop_size=224,
+)
+test_cfg = dict(
+    crop_size=224,
+    crop_type='normal'
+)
