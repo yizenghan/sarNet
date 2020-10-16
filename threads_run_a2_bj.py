48bfb2f97829a28891233e5f850db1e65cc21622
Yizeng Han <yizeng38@gmail.com>
2020-10-16 22:35:12 +0800

    thread_run_a2_bj for new code

--- threads_run_a2_bj.py
+++ threads_run_a2_bj.py
@@ -185,37 +185,37 @@ class myThread(threading.Thread):
                 width = self.width,
                 warmup=self.warmup,
                 test_code=self.test_code,
-                temp_scheduler=self.temp_scheduler
+                temp_scheduler=self.temp_scheduler,
                 gpu=self.gpu)
         os.system(cmd)
 
 
-config1 = f'_sarResNet50_g{args.patch_groups1}'
-config2 = f'_sarResNet50_g{args.patch_groups2}'
+config1 = f'_sarResNet50_g{args.patch_groups1}a{args.alpha1}s{args.base_scale1}_ls_warmup'
+config2 = f'_sarResNet50_g{args.patch_groups2}a{args.alpha2}s{args.base_scale2}_ls_warmup'
 
-if args.stage2 > 0:
-    config1 += '_s2'
-    config2 += '_s2'
+# if args.stage2 > 0:
+#     config1 += '_s2'
+#     config2 += '_s2'
 
-if args.self_mask1:
-    config1 += f'_selfmask_a{args.alpha1}b1_blConfig'
-else:
-    config1 += f'_a{args.alpha1}b1_blConfig'
+# if args.self_mask1:
+#     config1 += f'_selfmask_a{args.alpha1}b1_blConfig'
+# else:
+#     config1 += f'_a{args.alpha1}b1_blConfig'
 
-if args.self_mask2:
-    config2 += f'_selfmask_a{args.alpha2}b1_blConfig'
-else:
-    config2 += f'_a{args.alpha2}b1_blConfig'
+# if args.self_mask2:
+#     config2 += f'_selfmask_a{args.alpha2}b1_blConfig'
+# else:
+#     config2 += f'_a{args.alpha2}b1_blConfig'
 
-if args.use_ls1:
-    config1 += '_ls'
-if args.use_ls2:
-    config2 += '_ls'
+# if args.use_ls1:
+#     config1 += '_ls'
+# if args.use_ls2:
+#     config2 += '_ls'
 
-if args.warmup1:
-    config1 += '_warmup'
-if args.warmup2:
-    config2 += '_warmup'
+# if args.warmup1:
+#     config1 += '_warmup'
+# if args.warmup2:
+#     config2 += '_warmup'
 
 t1 = myThread(threadID=1,
                 config=config1, 
