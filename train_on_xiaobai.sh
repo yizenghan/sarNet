79b182bd19ab7098a428db0e774e82ede48a6849
Yizeng Han <yizeng38@gmail.com>
2020-10-19 18:49:56 +0800

    r34 and 50 threads run

--- train_on_xiaobai.sh
+++ train_on_xiaobai.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=4,5,6,7 \
 python main_sar_nomask.py \
 --test_code 0 \
---config configs/_uniform_res50_g1a2_ls_warmup.py \
+--config configs/_r50_ls_warmup.py \
 --train_url ./log/test2/ \
 --no_train_on_cloud \
 --use_amp 0 \
