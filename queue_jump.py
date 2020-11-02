a5f77a99b93b2ebed751dc370feb3d7ecb8ee3a6
thuallen <yizeng38@gmail.com>
2020-11-02 20:26:29 +0800

    modifying code

--- queue_jump.py
+++ queue_jump.py
@@ -6,7 +6,7 @@ def main():
 def check_gpu_memory():
     pynvml.nvmlInit()
 
-    gpulist = [4,5,6,7]
+    gpulist = [0,1,2,3]
     deviceCount = pynvml.nvmlDeviceGetCount()
     
     print('There are {} visiable GPUs'.format(deviceCount))
