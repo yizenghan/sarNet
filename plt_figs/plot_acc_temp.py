f086056be8e37ab0c494efdf94c2d02810009172
thuallen <yizeng38@gmail.com>
2020-11-02 16:15:43 +0800

    plt figs

--- plt_figs/plot_acc_temp.py
+++ plt_figs/plot_acc_temp.py
@@ -3,49 +3,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pylab import *
 
-epochs = np.array([e for e in range(160)])
+epochs = np.array([e for e in range(300)])
 
 
 fig = plt.figure()
-path1_1 = '/data/hanyz/code/SAR_zyt/log/__tempalphaBase_cifar16x4_g2_a2_s2_t0_1_0_target0_0_optimizeFromEpoch201_lambda_0_0_dynamicRate0/log.txt'
+path1_1 = '/data/hanyz/code/sarNet/log/zyt/cifar100/sar_resnet50_alphaBase_4stage_cifar/_round1_optimRate_g2_a2b1_s2/t0_1_0_tLast0_01_tempScheduler_exp_target0_7_optimizeFromEpoch90to150_dr2_lambda_1_0/log.txt'
 res1_1 = pd.read_csv(path1_1)
-acc1_1 = np.array(res1_1['val_acc_top1'])[:-1]
-rate1_1 = np.array(res1_1['val_act_rate'])
+acc1_1 = np.array(res1_1['train_loss'])[:-1]
+rate1_1 = np.array(res1_1['lr_log'])
 rate1_1 = rate1_1[:-1]
-rate1_1 = [r.replace('tensor(','') for r in rate1_1]
-rate1_1 = [r.replace(", device='cuda:0')",'') for r in rate1_1]
-rate1_1 = [float(r) for r in rate1_1]
-rate1_1 = np.array(rate1_1)
+# rate1_1 = [r.replace('tensor(','') for r in rate1_1]
+# rate1_1 = [r.replace(", device='cuda:0')",'') for r in rate1_1]
+# rate1_1 = [float(r) for r in rate1_1]
+# rate1_1 = np.array(rate1_1)
 
-path1_2 = '/data/hanyz/code/SAR_zyt/log/__tempalphaBase_cifar16x4_g2_a2_s2_t0_1_0_target0_25_optimizeFromEpoch100_lambda_0_1_dynamicRate0/log.txt'
+path1_2 = '/data/hanyz/code/sarNet/log/zyt/cifar100/sar_resnet50_alphaBase_4stage_cifar/_round1_optimRate_g2_a2b1_s2/t0_1_0_tLast0_01_tempScheduler_exp_target0_4_optimizeFromEpoch90to150_dr2_lambda_1_0/log.txt'
 res1_2 = pd.read_csv(path1_2)
-acc1_2 = np.array(res1_2['val_acc_top1'])[:-1]
-rate1_2 = np.array(res1_2['val_act_rate'])[:-1]
-rate1_2 = [r.replace('tensor(','') for r in rate1_2]
-rate1_2 = [r.replace(", device='cuda:0')",'') for r in rate1_2]
-rate1_2 = [float(r) for r in rate1_2]
-rate1_2 = np.array(rate1_2)
+acc1_2 = np.array(res1_2['train_loss'])[:-1]
+rate1_2 = np.array(res1_2['lr_log'])[:-1]
+# rate1_2 = [r.replace('tensor(','') for r in rate1_2]
+# rate1_2 = [r.replace(", device='cuda:0')",'') for r in rate1_2]
+# rate1_2 = [float(r) for r in rate1_2]
+# rate1_2 = np.array(rate1_2)
 
 
-plt.plot(epochs, rate1_1, c = '#ff3300', label='activation rate, t0=1.0, no constraint')
-plt.plot(epochs, rate1_2, c = '#ff99cc', label='activation rate, t0=1.0, target=0.25')
+plt.plot(epochs, rate1_1, c = '#ff3300', label='lr, t0=1.0, target=0.7')
+plt.plot(epochs, rate1_2, c = '#ff99cc', label='lr, t0=1.0, target=0.4')
 xlabel(r'$epoch$')
-ylabel(r'$activation rate$')
+ylabel(r'$lr$')
 plt.legend(loc=2)
 
 twinx()
 
-plt.plot(epochs, acc1_1, c = '#0066ff', label='acc1, t0=1.0, no constraint')
-plt.plot(epochs, acc1_2, c = '#33ccff', label='acc1, t0=1.0, target=0.25')
-ylabel(r'$acc1$')
+plt.plot(epochs, acc1_1, c = '#0066ff', label='train_loss, t0=1.0, target=0.7')
+plt.plot(epochs, acc1_2, c = '#33ccff', label='train_loss, t0=1.0, target=0.4')
+ylabel(r'$train_loss$')
 plt.legend(loc=3)
-plt.savefig('model1_temp1.png')
+plt.savefig('cifar100_loss_lr.png')
 
 
 # -----------------------------------------------------------------------------------------
 
 
-fig = plt.figure()
+'''fig = plt.figure()
 path2_1 = '/data/hanyz/code/SAR_zyt/log/__tempalphaBase_cifar16x4_g2_a2_s2_t0_5_0_target0_0_optimizeFromEpoch201_lambda_0_0_dynamicRate0/log.txt'
 res2_1 = pd.read_csv(path2_1)
 acc2_1 = np.array(res2_1['val_acc_top1'])[:-1]
@@ -116,4 +116,4 @@ plt.plot(epochs, acc2_2, c = '#33ccff', label='acc1, t0=10.0, target=0.25')
 ylabel(r'$acc1$')
 
 plt.legend(loc=3)
-plt.savefig('model1_temp10.png')
+plt.savefig('model1_temp10.png')'''
